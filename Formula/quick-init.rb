b30aae8e5a0fea3e15d59c9922f126f8dc7a42f9
OZCAP <48883102+OZCAP@users.noreply.github.com>
2022-07-28 19:13:40 +0200

    🐛 Fix incorrect sha hash

--- Formula/quick-init.rb
+++ Formula/quick-init.rb
@@ -2,7 +2,7 @@ class QuickInit < Formula
     desc "React project generator with auto Tailwind configuration"
     homepage "https://github.com/ozcap/quick-init"
     url "https://github.com/OZCAP/quick-init/releases/tag/v0.1.0/quick-init-mac.tar.gz"
-    sha256 "2477334ad653cf17ce4361b52f0d4014c6f6d852cb2a990efb1c8f843b5ed62e"
+    sha256 "c760a0269f824ea478207212fd960a4de7747e6276bc36d50fb994144c525392"
     version "0.1.0"
   
     def install
