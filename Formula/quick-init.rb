fa03683239e374e7ba4aad20855d9a522f2e3ba2
OZCAP <48883102+OZCAP@users.noreply.github.com>
2022-07-28 19:35:37 +0200

    🐛 Edit SHA

--- Formula/quick-init.rb
+++ Formula/quick-init.rb
@@ -1,8 +1,8 @@
 class QuickInit < Formula
     desc "React project generator with auto Tailwind configuration"
     homepage "https://github.com/ozcap/quick-init"
-    url "https://github.com/OZCAP/quick-init/releases/tag/v0.1.0/quick-init-mac.tar.gz"
-    sha256 "7416f77136584218a4dd3624892ffe0d4b1d25a5cb4562d3ad75d434ce8c2c03"
+    url "https://github.com/OZCAP/quick-init/releases/download/v0.1.0/quick-init-mac.tar.gz"
+    sha256 "35fd6f7e61dbe571b638fe4e493c93af48a46dab4550045ba7eca428ce50e18b"
     version "0.1.0"
   
     def install
