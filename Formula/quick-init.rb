95e62b6def95f12dd143b49e8f53a2575dbddcfb
Oscar <48883102+OZCAP@users.noreply.github.com>
2022-07-30 16:24:11 +0200

    :sparkles: Update version 0.2.0

--- Formula/quick-init.rb
+++ Formula/quick-init.rb
@@ -1,9 +1,9 @@
 class QuickInit < Formula
     desc "React project generator with auto Tailwind configuration"
     homepage "https://github.com/ozcap/quick-init"
-    url "https://github.com/OZCAP/quick-init/releases/download/v0.1.0/quick-init-mac.tar.gz"
-    sha256 "35fd6f7e61dbe571b638fe4e493c93af48a46dab4550045ba7eca428ce50e18b"
-    version "0.1.0"
+    url "https://github.com/OZCAP/quick-init/releases/download/v0.2.0/quick-init-mac.tar.gz"
+    sha256 "cc625e71af1e02b6492cde7ac0e071c530bc1c989711dbaa373372d823b77794"
+    version "0.2.0"
   
     def install
       bin.install "quick-init"
