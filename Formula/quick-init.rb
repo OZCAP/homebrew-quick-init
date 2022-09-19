f5a3e4e0cea435bba99965c1ef6f65ff6c55ee04
Oscar <48883102+OZCAP@users.noreply.github.com>
2022-09-19 23:28:55 +0200

    Add formula dependency

--- Formula/quick-init.rb
+++ Formula/quick-init.rb
@@ -4,6 +4,8 @@ class QuickInit < Formula
     url "https://github.com/OZCAP/quick-init/releases/download/v0.3.0/quick-init-mac.tar.gz"
     sha256 "c00e2f27b1e0ffad82845c74a0c1fb292be21bd7f0ce9d6bfaf49985af21639f"
     version "0.3.0"
+    
+    depends_on "node"
   
     def install
       bin.install "quick-init"
