428757e7020820a2690fc76122c916dc0a41faba
Oscar <48883102+OZCAP@users.noreply.github.com>
2022-09-19 12:14:29 +0200

    Update quick-init.rb

--- Formula/quick-init.rb
+++ Formula/quick-init.rb
@@ -2,7 +2,7 @@ class QuickInit < Formula
     desc "React/Next.js project generator with automatic Tailwind configuration"
     homepage "https://github.com/ozcap/quick-init"
     url "https://github.com/OZCAP/quick-init/releases/download/v0.3.0/quick-init-mac.tar.gz"
-    sha256 "d5489c43c899d4d1d4cf5d00c0ee8782a3bacc218de7bd1b237f322f479efeda"
+    sha256 "35fd6f7e61dbe571b638fe4e493c93af48a46dab4550045ba7eca428ce50e18b"
     version "0.3.0"
   
     def install
